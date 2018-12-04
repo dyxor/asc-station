30654801f137aa47e50db622342ddb135b25bf98
dy <dymain@126.com>
2018-12-04 18:57:20 +0800

    sql-modify

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// ---------------------------- functions -----------------------------------
+// --------------------------- functions -----------------------------------
 function pr($x){
   echo "<pre>", var_dump($x), "</pre>";
 }
@@ -8,7 +8,7 @@ function pr($x){
 if(!isset($_POST['action']))die("Refused.");
 
 
-// --------------------------------------------------------------------
+// --------------------------- init ----------------------------------
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
@@ -21,4 +21,13 @@ $db = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+// --------------------------------------------------------------------
+
+
+$sql = $_POST['order'];
+
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
