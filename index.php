078a9bb442e0925eb53237cc30ac9f545f49a0cd
dy <dymain@126.com>
2018-12-04 16:46:39 +0800

    test-done

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 function pr($x){
-  echo "<pre>".var_dump($x)."</pre>";
+  echo "<pre>", var_dump($x), "</pre>";
 }
 
-pr($_SERVER);
-pr($_GET);
-pr($_POST);
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
+$name = $_GET['name'];
+
+
+$arr = array('a' => 1, 'b' => 2, 'c' => 666, 'd' => 4, 'e' => 5);
+echo json_encode($arr);
